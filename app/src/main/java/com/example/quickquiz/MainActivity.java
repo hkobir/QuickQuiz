10728ea6f9dfafcefc3dad7773424b76b743478f
H.Kobir <hkobir10@gmail.com>
2020-12-11 21:41:43 +0600

    home screen and initial setup done

---
+++ app/src/main/java/com/example/quickquiz/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.quickquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
